e109ccaa9c2f4587c99f2d0d6b9206b52aee54b3
prabhjyot009 <prabh156dps@gmail.com>
2023-07-08 00:10:54 +0530

    commit3

---
+++ Loops/StreamOfIntegers.java
@@ -0,0 +1,32 @@
+package Loops;
+
+import java.util.Scanner;
+
+// public class StreamOfIntegers {
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         System.out.print("Enter number:");
+//         int n=sc.nextInt();
+//         int sum=0;
+//         while(n != -1){
+//             sum+=n;
+//             n=sc.nextInt();
+//         }
+//     System.out.println(sum);
+//     }
+// }
+
+//using do While
+public class StreamOfIntegers {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter number:");
+        int n=sc.nextInt();
+        int sum=0;
+        do{
+            sum+=n;
+            n=sc.nextInt();
+        }while(n != -1);
+    System.out.println(sum);
+    }
+}
