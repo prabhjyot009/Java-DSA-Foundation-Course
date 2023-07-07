a6ac7c2dd2e0a60f0fb90892b85f6689d458f6fa
prabhjyot009 <prabh156dps@gmail.com>
2023-07-08 01:17:13 +0530

    commit7

---
+++ Loops/Factorial.java
@@ -0,0 +1,17 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class Factorial {
+        public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int fact=1;
+        int i=1;
+        while(i<=n){
+            fact=fact*i;
+            i++;
+        }
+        System.out.println(fact);
+    }
+}
---
+++ Loops/ReverseDigits.java
@@ -0,0 +1,16 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class ReverseDigits {
+        public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int rev=0;
+        while(n>0){
+            rev=(rev*10)+n%10;
+            n=n/10;
+        }
+        System.out.println(rev);
+    }
+}
