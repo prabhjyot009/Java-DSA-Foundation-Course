2ca5707ac47c670e05ffc4dd3afa1f4997c3d42f
prabhjyot009 <prabh156dps@gmail.com>
2023-07-11 00:10:07 +0530

    Methods in java

---
+++ OOPSconcept/FloorCeil.java
@@ -0,0 +1,12 @@
+package OOPSconcept;
+
+public class FloorCeil {
+    public static void main(String[] args) {
+        //floor
+        System.out.println(Math.floor(5.3));
+        System.out.println(Math.floor(5.8));
+        //ceil:
+        System.out.println(Math.ceil(5.3));
+        System.out.println(Math.ceil(5.8));
+    }
+}
