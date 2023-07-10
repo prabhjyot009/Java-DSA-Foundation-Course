38e7463c05deb822a51159a4685585638feff960
prabhjyot009 <prabh156dps@gmail.com>
2023-07-11 02:46:43 +0530

    Methods and Scope concept completed

--- OOPSconcept/MethodsAndScopes/BlockLevelScope.java
+++ OOPSconcept/MethodsAndScopes/BlockLevelScope.java
@@ -1,20 +1,31 @@
 package OOPSconcept.MethodsAndScopes;
+    class Algebraa{
 
+        void demo(){
+            int a=5;
+            System.out.println(a);
+            //error: System.out.println(b);
+            for (int i = 0; i < 5; i++) //loop in block level
+            {
+                int b=10;
+                System.out.println(b); //10
+            }
+            //Error: System.out.println(i);
+            //error: System.out.println(b);
+            if(true)
+            {
+                int b=100;
+                System.out.println(b); //100
+            }
+            //We can create new scope for i because scope is changed
+            for(int i=0;i<2;i++){
+
+            }
+        }
+    }
 public class BlockLevelScope {
-    class Algebra{
-        int a=10;//member variables
-        int b=20;
-    
-        int add(){
-            return a+b;
-        }
-        
-        int sub(){
-            return a-b;
-        }
-        
-        int mul(){
-            return a*b;
-        }
+    public static void main(String[] args) {
+        Algebraa obj=new Algebraa();
+        obj.demo();
     }
 }
