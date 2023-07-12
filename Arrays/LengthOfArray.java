78acd1a1e88f9d3ee0990b48111c41d83608f4a7
prabhjyot009 <prabh156dps@gmail.com>
2023-07-13 03:11:20 +0530

    commit2

--- Arrays/LengthOfArray.java
+++ Arrays/LengthOfArray.java
@@ -1,6 +1,5 @@
 package Arrays;
-
-class Arrayss{
+class lengthArray{
     void Arraysss(){
         int[] ages=new int[3];
         float[] weights=new float[3];
@@ -9,14 +8,15 @@ class Arrayss{
         ages[0]=12;
         ages[1]=29;
         ages[2]=56;
+        System.out.println(ages.length);
         System.out.println(ages[0]);
         System.out.println(ages[1]);
         System.out.println(ages[2]);
     }
 }
-public class DemoArrays {
+public class LengthOfArray {
     public static void main(String[] args) {
-        Arrayss obj=new Arrayss();
+        lengthArray obj=new lengthArray();
         obj.Arraysss();
     }
 }
---
+++ Arrays/TwoDArray.java
@@ -0,0 +1,25 @@
+package Arrays;
+
+class mularrays{
+    void multiarrays(){
+        //int[][] ages=new int[2][4];
+        int[][] ages={{1,2,3},{4,5,6},{7,8,9}};
+        System.out.println(ages[0][0]);
+        System.out.println(ages[0][1]);
+        System.out.println(ages[0][2]);
+
+        System.out.println(ages[1][0]);
+        System.out.println(ages[1][1]);
+        System.out.println(ages[1][2]);
+        
+        System.out.println(ages[2][0]);
+        System.out.println(ages[2][1]);
+        System.out.println(ages[2][2]);
+    }
+}
+public class TwoDArray {
+    public static void main(String[] args) {
+        mularrays obj=new mularrays();
+        obj.multiarrays();
+    }
+}
