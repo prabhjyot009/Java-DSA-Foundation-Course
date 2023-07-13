abb07bdb50da9542c9faac6ecdc31e01c086bf94
prabhjyot009 <prabh156dps@gmail.com>
2023-07-13 16:03:53 +0530

    commit4

--- Arrays/SearchElement.java
+++ Arrays/SearchElement.java
@@ -1,16 +1,24 @@
 package Arrays;
-class maxArray{
-    void maxx(){
-        int[] arr={81,3,6,2,8,1};
-        int max=arr[0];
+class searchArray{
+    void searchh(){
+        int[] arr={81,3,6,2,8,3,1};
+        int search=3;
+        int ans=-1;
         for(int i=0;i<arr.length;i++){
-            if(arr[i]>max){
-                max=arr[i];
+            if(arr[i]==search){
+                ans=i;
+                break;
             }
         }
-        System.out.print("Maximum is:"+max);
-    }
+        if(ans==-1){
+            System.out.print("Not found");
+        }else{
+        System.out.print(search+" Item found at index:"+ans);
+    }}
 }
 public class SearchElement {
-    
+    public static void main(String[] args) {
+        searchArray obj=new searchArray();
+        obj.searchh();
+    }
 }
