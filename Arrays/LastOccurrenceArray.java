b48e8cb17b11afcd045c9cf16ffda8f31f0e833f
prabhjyot009 <prabh156dps@gmail.com>
2023-07-14 23:43:19 +0530

    commit9

--- Arrays/LastOccurrenceArray.java
+++ Arrays/LastOccurrenceArray.java
@@ -8,20 +8,20 @@ public class LastOccurrenceArray {
     System.out.println();
     }
     static void LastOccurrencesArr(int[] arr){
-        int count=0;
+        int lastIndex=-1;
         int target=5;
         for (int i = 0; i < arr.length; i++) {
             if(arr[i]==target){
-                count+=1;
+                lastIndex=i;
             }
         }
-        System.out.println(count);
+        System.out.println(lastIndex);
     }
     public static void main(String[] args) {
         int[] arr={5,7,8,5,9,8,0};
         System.out.println("Orignal Array:");
         printArray(arr);
-        System.out.println("Number of occurences in an array are:");
+        System.out.println("Last occurences is:");
         LastOccurrencesArr(arr);
     }
 }
