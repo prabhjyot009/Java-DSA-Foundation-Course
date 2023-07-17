e8e8be1a64193e79fe8d172d6466ccf93c8a315e
prabhjyot009 <prabh156dps@gmail.com>
2023-07-17 23:46:07 +0530

    commit16

--- Arrays/TargetSum/PairsInArray.java
+++ Arrays/TargetSum/PairsInArray.java
@@ -3,13 +3,16 @@ package Arrays.TargetSum;
 public class PairsInArray {
     static void printSum(int[] arr){
         for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]+" ");
+            System.out.print(arr[i]+" ");
         }
         System.out.println();
     }
     static void targetSum(int[] arr,int x){
         for (int i = 0; i < arr.length; i++) {
-
+            for (int j = i+1; j < arr.length; j++) {
+                if(arr[i]+arr[j]==x){
+                    System.out.println("target"+arr[i]+" "+arr[j]);
+                }
             }
         }
     }
