ee7cadb36b131eaa9c2c65cb0ad02b4f8190101e
prabhjyot009 <prabh156dps@gmail.com>
2023-07-19 23:30:47 +0530

    commit

---
+++ Arrays/ArrayManipulation/SecondLargestArray.java
@@ -0,0 +1,27 @@
+package Arrays.ArrayManipulation;
+
+public class SecondLargestArray {
+    static void printArray(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    static void secondLargest(int[] arr){
+        int max=0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length-1; j++) {
+                if (arr[j]>max) {
+                    max=arr[j];
+                    
+                }
+            }
+        }
+        System.out.println(max);
+    }
+    public static void main(String[] args) {
+        int[] arr={8,3,5,9,2,0};
+        printArray(arr);
+        secondLargest(arr);
+    }
+}
