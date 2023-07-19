8e0df91f94181bcf298af921720c77e024f31317
prabhjyot009 <prabh156dps@gmail.com>
2023-07-19 23:54:40 +0530

    commit

--- Arrays/ArrayManipulation/RepeatingArray.java
+++ Arrays/ArrayManipulation/RepeatingArray.java
@@ -8,20 +8,18 @@ public class RepeatingArray {
         System.out.println();
     }
     static int repeatArray(int[] arr){
-        int repeat=0;
         for (int i = 0; i < arr.length; i++) {
             for (int j = i+1; j <arr.length; j++) {
                 if (arr[i]==arr[j]) {
-                    repeat=arr[i];
+                    return arr[i];
                 }
             }
         }
-        System.out.println(repeat);
-        return repeat;
+        return -1;
     }
     public static void main(String[] args) {
-        int[] arr={8,8,5,9,2,0};
+        int[] arr={8,9,5,9,2,0};
         printArray(arr);
-        repeatArray(arr);
+        System.out.println(repeatArray(arr));
     }
 }
