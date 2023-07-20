b88e582fdc332d913050dedbaa5c9c68a8c0e335
prabhjyot009 <prabh156dps@gmail.com>
2023-07-20 23:34:49 +0530

    commit

--- Arrays/SwappingArray.java
+++ Arrays/SwappingArray.java
@@ -1,5 +1,17 @@
 package Arrays;
 
 public class SwappingArray {
-    
+    static void printArray(int[] arr){
+
+    }
+    static void swapitem(int a,int b){
+        int temp=a;
+        a=b;
+        b=temp;
+        System.out.println(b);
+    }
+    public static void main(String[] args) {
+        int[] arr={1,2};
+        swapitem(2,3);
+    }
 }
