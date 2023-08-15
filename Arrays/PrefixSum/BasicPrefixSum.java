38fefe207b420c64f590d98b705d8a20b7bdc633
prabhjyot009 <prabh156dps@gmail.com>
2023-08-15 21:02:45 +0530

    Max Prefix sum

---
+++ Arrays/PrefixSum/BasicPrefixSum.java
@@ -0,0 +1,29 @@
+package Arrays.PrefixSum;
+
+public class BasicPrefixSum {
+
+    static void printArray(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+
+    static int[] maxPrefixSum(int[] arr){
+        int n=arr.length;
+        int[] prefix=new int[n];
+        prefix[0]=arr[0];
+        for (int i = 1; i < n; i++) {
+            prefix[i]=prefix[i-1]+arr[i];
+        }
+        return prefix;
+    }
+    public static void main(String[] args) {
+        int[] arr={2,1,3,4,5};
+        System.out.println("Orignal array:");
+        printArray(arr);
+        int[] prefix=maxPrefixSum(arr);
+        System.out.println("Max Prefix sum");
+        printArray(prefix);
+    }
+}
