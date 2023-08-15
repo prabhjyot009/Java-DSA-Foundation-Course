818d7d1f5bf87801869cb323b787607a5ae0bbe5
prabhjyot009 <prabh156dps@gmail.com>
2023-08-15 10:26:52 +0530

    Zeroes and Ones normal approach sorting

---
+++ Arrays/TwoPointers/ZeroesANDOnesSort.java
@@ -0,0 +1,56 @@
+//normal approach
+package Arrays.TwoPointers;
+
+import java.util.Scanner;
+
+public class ZeroesANDOnesSort {
+
+    static void printArray(int arr[]){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+
+    static void swapZeroesAndOnes(int[] arr){
+        int n=arr.length;
+        int zeroes=0;
+        for (int i = 0; i < n; i++) {
+            if(arr[i]==0){
+                zeroes++;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if(i<zeroes){
+                arr[i]=0;
+            }
+            else{
+                arr[i]=1;
+            }
+        }
+    }
+
+    static void swap(int[] arr,int i, int j){
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter size of array");
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+
+        System.out.println("Enter "+n+"elements");
+        for (int i = 0; i < n; i++) {
+            arr[i]=sc.nextInt();
+        }
+
+        System.out.println("Orignal array:");
+        printArray(arr);
+        swapZeroesAndOnes(arr);
+        System.out.println("Sorted Array");
+        printArray(arr);
+
+    }
+}
