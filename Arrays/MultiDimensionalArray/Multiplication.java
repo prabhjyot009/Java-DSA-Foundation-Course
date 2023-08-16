8cb9e68e43ec4632be78d3816697efffbdc6d044
prabhjyot009 <prabh156dps@gmail.com>
2023-08-16 23:17:01 +0530

    Addition in matrix

--- Arrays/MultiDimensionalArray/Multiplication.java
+++ Arrays/MultiDimensionalArray/Multiplication.java
@@ -2,7 +2,7 @@ package Arrays.MultiDimensionalArray;
 
 import java.util.Scanner;
 
-public class Addition {
+public class Multiplication {
     static void printArray(int[][] arr) {
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
