b4d3deeb9ed1fa0dd89a65c5e55f9a22837ff747
prabhjyot009 <prabh156dps@gmail.com>
2023-08-16 18:52:34 +0530

    User Input of 2 d array

--- Arrays/MultiDimensionalArray/UserInputTwoDArray.java
+++ Arrays/MultiDimensionalArray/UserInputTwoDArray.java
@@ -1,5 +1,33 @@
 package Arrays.MultiDimensionalArray;
 
+import java.util.Scanner;
+
 public class UserInputTwoDArray {
-    
+
+    static void printArray(int[][] arr){
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter number of rows");
+        int r=sc.nextInt();
+        System.out.println("Enter number of colomns");
+        int c=sc.nextInt();
+
+        int[][] arr=new int[r][c];
+
+        System.out.println("Enter "+r*c+"elements");
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        System.out.println("2-D array:");
+        printArray(arr);
+    }
 }
