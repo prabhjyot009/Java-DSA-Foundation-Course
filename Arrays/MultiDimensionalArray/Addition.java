87132f8f9cfd3e8572f265a98645127004cc6ac5
prabhjyot009 <prabh156dps@gmail.com>
2023-08-16 22:57:31 +0530

    Addition in matrix

--- Arrays/MultiDimensionalArray/Addition.java
+++ Arrays/MultiDimensionalArray/Addition.java
@@ -14,37 +14,40 @@ public class Addition {
     static void sumArray(int[][] a,int r1,int c1,int[][] b,int r2,int c2){
         if(r1!=r2 && c1!=c2){
             System.out.println("Wrong input - Addition not possible");
+            return;
         }
-        int[][] sum=new int[r1][c1]
+        int[][] sum=new int[r1][c1];
         for (int i = 0; i < r1; i++) {//rows
             for (int j = 0; j < c1; j++) {//colos
-                sum[i][j]=s
+                sum[i][j]=a[i][j]+b[i][j];
             }
         }
+        System.out.println("Sum of Matrix A and Matrix B");
+        printArray(sum);
     }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter number of rows");
+        System.out.println("Enter number of rows for matrix 1");
         int r1=sc.nextInt();
-        System.out.println("Enter number of colomns");
+        System.out.println("Enter number of colomns for matrix 1");
         int c1=sc.nextInt();
 
         int[][] a=new int[r1][c1];
 
-        System.out.println("Enter "+r1*c1+"elements");
+        System.out.println("Enter "+r1*c1+" elements");
         for (int i = 0; i < r1; i++) {
             for (int j = 0; j < c1; j++) {
                 a[i][j]=sc.nextInt();
             }
         }
-        System.out.println("Enter number of rows");
+        System.out.println("Enter number of rows for matrix 2");
         int r2=sc.nextInt();
-        System.out.println("Enter number of colomns");
+        System.out.println("Enter number of colomns for matrix 2");
         int c2=sc.nextInt();
 
         int[][] b=new int[r2][c2];
 
-        System.out.println("Enter "+r2*c2+"elements");
+        System.out.println("Enter "+r2*c2+" elements");
         for (int i = 0; i < r2; i++) {
             for (int j = 0; j < c2; j++) {
                 b[i][j]=sc.nextInt();
@@ -54,6 +57,6 @@ public class Addition {
         printArray(a);
         System.out.println("Matrix B:");
         printArray(b);
-        
+        sumArray(a, r1, c1, b, r2, c2);
     }
 }
