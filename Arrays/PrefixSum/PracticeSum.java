aafbc9085acc16274dd243958ff1a00ca0b84cf6
prabhjyot009 <prabh156dps@gmail.com>
2023-08-17 23:15:25 +0530

    commit

--- Arrays/PrefixSum/PracticeSum.java
+++ Arrays/PrefixSum/PracticeSum.java
@@ -3,10 +3,37 @@ package Arrays.PrefixSum;
 import java.util.Scanner;
 
 public class PracticeSum {
+    static int findTotalSum(int[] arr){
+        int totalSum=0;
+        for (int i = 0; i < arr.length; i++) {
+            totalSum+=arr[i];
+        }
+        return totalSum;
+    }
+
+    static boolean partitionSum(int[] arr){
+        int prefixSum=0;
+        int totalSum=findTotalSum(arr);
+        for (int i = 0; i < arr.length; i++) {
+            prefixSum+=arr[i];
+            int suffixSum=totalSum+prefixSum;
+            if (suffixSum==prefixSum) {
+                return true;
+            }
+        }
+        return false;
+    }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter size of array:");
         int n=sc.nextInt();
-        System.out.println("Enter "+);
+        int[] arr=new int[n];
+        System.out.println("Enter "+n+" Elements");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]=sc.nextInt();
+        }
+
+        System.out.println("Orignal Array:");
+        printArray(arr);
     }
 }
