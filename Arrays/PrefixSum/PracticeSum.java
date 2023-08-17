413d4dfc6c74465e3b0c2aa44d7a31e17ca53d14
prabhjyot009 <prabh156dps@gmail.com>
2023-08-17 23:08:53 +0530

    commit

---
+++ Arrays/PrefixSum/PracticeSum.java
@@ -0,0 +1,12 @@
+package Arrays.PrefixSum;
+
+import java.util.Scanner;
+
+public class PracticeSum {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter size of array:");
+        int n=sc.nextInt();
+        System.out.println("Enter "+);
+    }
+}
