10614693a702e99d36d0d8353735106988dac078
prabhjyot009 <prabh156dps@gmail.com>
2023-08-24 00:36:17 +0530

    commmit

--- Arrays/Sorting/Practice.java
+++ Arrays/Sorting/Practice.java
@@ -1,5 +1,34 @@
 package Arrays.Sorting;
 
 public class Practice {
-    
+    static void printArray(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    static int majorityElement(int[] arr){
+        // int freq=0;
+        // int element=0;
+        // for (int i = 0; i < arr.length; i++) {
+        //     int count=0;
+        //     for (int j = 0; j < arr.length; j++) {
+        //         if(arr[i]==arr[j]){
+        //             count++;
+        //         }
+        //     }
+        //     if(count>freq){
+        //         freq=count;
+        //         element=arr[i];
+        //     }
+        // }
+        // System.out.println("Element is "+element);
+        // System.out.println("Frequency is "+freq);
+        // return element;
+    }
+    public static void main(String[] args) {
+        int[] arr={1,2,3,3,4,3};
+        printArray(arr);
+        majorityElement(arr);
+    }
 }
