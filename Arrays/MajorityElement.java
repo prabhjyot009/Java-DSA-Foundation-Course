2cc5114891b1eb1e2d44d2cd2ba4ba6c68073150
prabhjyot009 <prabh156dps@gmail.com>
2023-08-25 13:15:20 +0530

    Majority Element

--- Arrays/MajorityElement.java
+++ Arrays/MajorityElement.java
@@ -1,33 +1,34 @@
 package Arrays;
+
 import java.io.*;
+
 public class MajorityElement {
 
-    static int majorityElement(int[] arr,int n){
-        int max_freq=0;
-        int index=-1;
+    static void majorityElement(int[] arr, int n) {
+        int max_freq = 0;
+        int index = 0;
         for (int i = 0; i < n; i++) {
-            int count=0;
-            for (int j = 0; j < n; j++) {
-                if(arr[i]==arr[j]){
-                    count+=1;
+            int count = 0;
+            for (int j = i; j < n; j++) {
+                if (arr[i] == arr[j]) {
+                    count += 1;
                 }
             }
-            if (count>max_freq) {
-                max_freq=count;
-                index=i;
+            if (count > max_freq) {
+                max_freq = count;
+                index = i;
             }
         }
-        if(max_freq>n/2){
-            System.out.println(arr[index]+" "+max_freq);
-        }
-        else{
+        if (max_freq > n / 2) {
+            System.out.println(arr[index] + " " + max_freq);
+        } else {
             System.out.println("No Majority Element");
         }
-        return index;
     }
-        public static void main(String[] args) {
-        int[] arr={89,9,9,9,9,1,1};
-        int n=arr.length;
-        majorityElement(arr,n);
+
+    public static void main(String[] args) {
+        int[] arr = { 1,2,1};
+        int n = arr.length;
+        majorityElement(arr, n);
     }
 }
