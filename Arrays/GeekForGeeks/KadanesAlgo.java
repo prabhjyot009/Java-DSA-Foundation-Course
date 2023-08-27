cdce084df3fbddb94fac3611b5355f98801749ce
prabhjyot009 <prabh156dps@gmail.com>
2023-08-28 02:59:56 +0530

    gfg

---
+++ Arrays/GeekForGeeks/KadanesAlgo.java
@@ -0,0 +1,33 @@
+package Arrays.GeekForGeeks;
+
+public class KadanesAlgo {
+    
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, -2, 5};
+        int n = arr.length;
+        KadanesAlgo obj = new KadanesAlgo();
+        int ans = obj.new Solution().maxSubarraySum(arr, n);
+        System.out.println(ans);
+    }
+
+    class Solution {
+        int maxSubarraySum(int arr[], int n) {
+            int maxSum = Integer.MIN_VALUE;
+            int currentSum = 0;
+
+            for (int i = 0; i < n; i++) {
+                currentSum += arr[i];
+
+                if (currentSum > maxSum) {
+                    maxSum = currentSum;
+                }
+
+                if (currentSum < 0) {
+                    currentSum = 0;
+                }
+            }
+
+            return maxSum;
+        }
+    }
+}
