5fca6c25b2fa43861fefd679a73e43a32d5556aa
prabhjyot009 <prabh156dps@gmail.com>
2023-08-29 22:20:44 +0530

    gfg

--- Arrays/Sorting/ZerosToEnd.java
+++ Arrays/Sorting/ZerosToEnd.java
@@ -1,6 +1,20 @@
 package Arrays.Sorting;
 
 public class ZerosToEnd {
-       int[] arr={5,0,6,1,0,3};
-       
+    public static void main(String[] args) {
+        int[] arr={5,0,6,1,0,3};
+        int n=arr.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n-i-1; j++) {
+                if(arr[j]==0 && arr[j+1]!=0){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        for (int i : arr) {
+            System.out.print(i+" ");
+        }
+    }
 }
