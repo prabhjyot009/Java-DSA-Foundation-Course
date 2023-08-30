104a437404a59c27c5fa8931aafa32946eeaf303
prabhjyot009 <prabh156dps@gmail.com>
2023-08-31 02:53:01 +0530

    gfg

--- Arrays/GeekForGeeks/CountPairsWithsSum.java
+++ Arrays/GeekForGeeks/CountPairsWithsSum.java
@@ -1,25 +1,34 @@
 package Arrays.GeekForGeeks;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class CountPairsWithsSum {
     public static void main(String[] args) {
         int[] arr={1,5,7,1};
-        System.out.println(countPairsWithsSum(arr));
-    }
-    //target 6 should return 2 provide me count of pairs whose sum is 6 code for output 2 is:
-    static int countPairsWithsSum(int[] arr){
         int target=6;
-        int ans=0;
-        int n=arr.length;
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(arr[i]+arr[j]==target){
-                    ans++;
-                }
+        System.out.println(countPairs(arr,target));
+    }
+    //using hashmaps O(n) time and O(n) space complexity
+    static int countPairs(int[] arr,int target){
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            if(map.containsKey(arr[i])){
+                map.put(arr[i],map.get(arr[i])+1);
+            }
+            else{
+                map.put(arr[i],1);
             }
         }
-        System.out.println("Number of pairs with the given sum are: "+ans);
-        return ans;
+        int count=0;
+        for (int i = 0; i < arr.length; i++) {
+            if(map.containsKey(target-arr[i])){
+                count+=map.get(target-arr[i]);
+            }
+            if(target-arr[i]==arr[i]){
+                count--;
+            }
+        }
+        return count/2;
     }
 }
