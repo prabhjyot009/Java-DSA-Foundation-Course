be449e63cbe7052c5cb4ea888a5d811f8a0a54d3
prabhjyot009 <prabh156dps@gmail.com>
2023-09-07 23:59:03 +0530

    Palindrome String

--- Strings/PalindromeSubStrings.java
+++ Strings/PalindromeSubStrings.java
@@ -2,6 +2,29 @@ package Strings;
 
 public class PalindromeSubStrings {
     public static void main(String[] args) {
-        
+        String str = "abc";
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = i + 1; j <= str.length(); j++) { 
+                String s = str.substring(i, j);
+                if (isPalindrome(s)) {
+                    count++;
+                    System.out.println(s);
+                }
+            }
+        }
+        System.out.println("Total Palindrome Substrings are: " + count);
+    }
+
+    static boolean isPalindrome(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+        while (i <= j) {
+            if (s.charAt(i) != s.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
     }
 }
