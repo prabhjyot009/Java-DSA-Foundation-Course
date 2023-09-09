4b3a3b84ffa831112a37ed6c35b8b84ae5823141
prabhjyot009 <prabh156dps@gmail.com>
2023-09-09 15:31:33 +0530

    import statements in oops

--- OOPSconcept/ImpConcepts/Test.java
+++ OOPSconcept/ImpConcepts/Test.java
@@ -1,10 +1,13 @@
 package OOPSconcept.ImpConcepts;
 
+import java.util.regex.Pattern; //import to import sub package
+
 public class Test {
     public static void main(String[] args) {
         String s=new String("HEllo");
         StringBuffer str=new StringBuffer(s);
         Student sc=new Student();
         Student.m1(); 
+        Pattern p=Pattern.compile("Ab");
     }
 }
