4b4c3812d0f047ad79e2eebe355e7afd4e5a3488
prabhjyot009 <prabh156dps@gmail.com>
2023-09-09 15:12:51 +0530

    Important oops concepts

---
+++ OOPSconcept/ImpConcepts/Student.java
@@ -0,0 +1,7 @@
+package OOPSconcept.ImpConcepts;
+
+public class Student {
+    public static void m1() {
+        System.out.println("Student class static method");
+    }
+}
---
+++ OOPSconcept/ImpConcepts/Test.java
@@ -0,0 +1,10 @@
+package OOPSconcept.ImpConcepts;
+
+public class Test {
+    public static void main(String[] args) {
+        String s=new String("HEllo");
+        StringBuffer str=new StringBuffer(s);
+        Student sc=new Student();
+        Student.m1(); 
+    }
+}
