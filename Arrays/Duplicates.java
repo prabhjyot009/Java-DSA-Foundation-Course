6c01db6faf099f6d32640980b80d6c2b854562e5
prabhjyot009 <prabh156dps@gmail.com>
2023-09-15 00:16:56 +0530

    Duplicates in array

--- Arrays/Duplicates.java
+++ Arrays/Duplicates.java
@@ -3,13 +3,14 @@ package Arrays;
 public class Duplicates {
     public static void main(String[] args) {
         int[] nums = { 1, 2, 3, 1, 3, 6, 6 };
-        int n = nums.length;
-        for (int i = 0; i < n; i++) {
-            int index = nums[i] % n;
-            if (nums[index] >= n) {
-                System.out.println(index);
-            } else {
-                nums[index] += n;
+        int[] hash = new int[7];
+        for (int i = 0; i < nums.length; i++) {
+            hash[nums[i]]++;
+        }
+
+        for (int i = 0; i < hash.length; i++) {
+            if (hash[i] > 1) {
+                System.out.print(i + " ");
             }
         }
     }
