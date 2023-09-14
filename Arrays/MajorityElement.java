47b4b6e6ff44302777be9e7bb67af43c948d8a5a
prabhjyot009 <prabh156dps@gmail.com>
2023-09-14 21:19:06 +0530

    majority element

--- Arrays/MajorityElement.java
+++ Arrays/MajorityElement.java
@@ -4,33 +4,24 @@ import java.io.*;
 import java.util.Scanner;
 
 public class MajorityElement {
-    public static int majorityElement1(int[] nums) {
-        int n = nums.length;
-        int majority = n/2;
-        int[] count = new int[100];
-        for(int num : nums){
-            count[num]++;
-            if(count[num] > majority){
-                return num;
-            }
-        }
-        return -1;
-    }
-
     public static void main(String args[])
     {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        
-        while(t-- > 0)
-        {
-            int n =sc.nextInt();
-            int arr[] = new int[n];
-            
-            for(int i = 0; i < n; i++)
-             arr[i] = sc.nextInt();
-             
-           System.out.println(new MajorityElement().majorityElement1(arr)); 
+        int[] nums={3,2,3};
+        int val=nums[0];
+        int count=1;
+        int n=nums.length;
+        for(int i=1;i<n;i++){
+            if(count==0){
+                val=nums[i];
+                count=1;
+            }
+            else if(nums[i]==val){
+                count+=1;
+            }
+            else{
+                count--;
+            }
         }
+        System.out.println(val);
     }
 }
