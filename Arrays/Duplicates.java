0fcf574a473ec580ac62559daaf73624373916b9
prabhjyot009 <prabh156dps@gmail.com>
2023-09-15 00:26:39 +0530

    Duplicates in java

--- Arrays/Duplicates.java
+++ Arrays/Duplicates.java
@@ -1,16 +1,16 @@
 package Arrays;
 
+import java.util.LinkedList;
+
 public class Duplicates {
     public static void main(String[] args) {
         int[] nums = { 1, 2, 3, 1, 3, 6, 6 };
-        int[] hash = new int[7];
+        LinkedList<Integer> list = new LinkedList<>();
         for (int i = 0; i < nums.length; i++) {
-            hash[nums[i]]++;
-        }
-
-        for (int i = 0; i < hash.length; i++) {
-            if (hash[i] > 1) {
-                System.out.print(i + " ");
+            if (list.contains(nums[i])) {
+                System.out.print(nums[i] + " ");
+            } else {
+                list.add(nums[i]);
             }
         }
     }
