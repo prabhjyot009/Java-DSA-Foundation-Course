76eee84392315a908440addb1867820cdaab27ba
prabhjyot009 <prabh156dps@gmail.com>
2023-09-15 22:22:09 +0530

    Duplicates in an array

--- Arrays/Duplicates.java
+++ Arrays/Duplicates.java
@@ -15,6 +15,25 @@ public class Duplicates {
     //     }
     // }
     public static void main(String[] args) {
-        int 
+        int[] arr={1, 2, 3, 1, 3, 6, 6 };
+        // int[] freq=new int[arr.length];
+        // for(int i=0;i<arr.length;i++){
+        //     freq[arr[i]]++;
+        // }
+        // for(int i=0;i<freq.length;i++){
+        //     if(freq[i]>1){
+        //         System.out.print(i+" ");
+        //     }
+        // }
+
+        //without using any extra space
+        for(int i=0;i<arr.length;i++){
+            int idx=Math.abs(arr[i]);
+            if(arr[idx]<0){
+                System.out.print(idx+" ");
+            }else{
+                arr[idx]=-arr[idx];
+            }
+        }
     }
 }
