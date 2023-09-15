46bc1ebb1364c5d09c6cd7cea0838776e5f8e288
prabhjyot009 <prabh156dps@gmail.com>
2023-09-16 00:39:16 +0530

    Increasing natural number using recursion

--- Recursion/PrintNatural.java
+++ Recursion/PrintNatural.java
@@ -6,8 +6,8 @@ public class PrintNatural {
             System.out.print(1);
             return;
         }
-        return n-1(n);
-        System.out.print(n);
+        printIncreasing(n-1);
+        System.out.print(" "+n+"");
     }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
