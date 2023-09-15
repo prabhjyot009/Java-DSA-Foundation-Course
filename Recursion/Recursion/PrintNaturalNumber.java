673325e5fd7131dd38deab3d6f58d7e189590df7
prabhjyot009 <prabh156dps@gmail.com>
2023-09-15 21:27:02 +0530

    Recursion Started

---
+++ Recursion/Recursion/PrintNaturalNumber.java
@@ -0,0 +1,15 @@
+package Recursion;
+
+import java.util.Scanner;
+
+/**
+ * PrintNaturalNumber
+ */
+public class PrintNaturalNumber {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        printIncreasing(n);
+    }
+}
