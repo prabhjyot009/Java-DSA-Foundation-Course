d8ed5c8d47063c9fd68de8308d12dff6f68bcb9a
prabhjyot009 <prabh156dps@gmail.com>
2023-09-16 00:07:52 +0530

    Print Natural number using recursion

--- Recursion/PrintNatural.java
+++ Recursion/PrintNatural.java
@@ -2,7 +2,12 @@ import java.util.Scanner;
 
 public class PrintNatural {
     static void printIncreasing(int n){
-        
+        if(n==1){
+            System.out.print(1);
+            return;
+        }
+        return n-1(n);
+        System.out.print(n);
     }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
