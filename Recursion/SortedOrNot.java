d8bcb1effcce1c70978e43d747d7956cfa100fcc
prabhjyot009 <prabh156dps@gmail.com>
2023-09-17 21:29:27 +0530

    Sorted or not using recursion

--- Recursion/SortedOrNot.java
+++ Recursion/SortedOrNot.java
@@ -1,5 +1,17 @@
 public class SortedOrNot {
+    static int sorted(int[] arr,int n,int idx){
+        if(idx==n-1){
+            return 1;
+        }
+        if(arr[idx]>arr[idx+1]){
+            return 0;
+        }
+        else{
+            return sorted(arr,n,idx+1);
+        }
+    }
     public static void main(String[] args) {
-        
+        int[] arr={1,2,3,4};
+        System.out.println(sorted(arr,arr.length,0));
     }
 }
