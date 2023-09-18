ab5adb8360ad15f0078937fdecc6efd8fe520400
prabhjyot009 <prabh156dps@gmail.com>
2023-09-19 00:59:08 +0530

    FInd Sum of character

---
+++ Strings/FirstLastCharacterUpper.java
@@ -0,0 +1,21 @@
+package Strings;
+
+public class FirstLastCharacterUpper {
+    public static void main(String[] args) {
+        String str = "take u forward is awesome";
+        String[] words = str.split(" ");
+        StringBuilder cap = new StringBuilder();
+        for (String w : words) {
+            if(w.length() >=2){
+                String first = w.substring(0, 1);
+                String afterFirst = w.substring(1, w.length() - 1);
+                String last = w.substring(w.length() - 1);
+                cap.append(first.toUpperCase()).append(afterFirst).append(last.toUpperCase()).append(" ");
+            }
+            else{
+                cap.append(w.toUpperCase()).append(" ");
+            }
+        }
+        System.out.println(cap);
+    }
+}
