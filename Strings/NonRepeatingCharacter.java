1094867953a6aaee53d610767112a35829b6fec1
prabhjyot009 <prabh156dps@gmail.com>
2023-09-19 21:12:10 +0530

    Non repeating characters in string

--- Strings/NonRepeatingCharacter.java
+++ Strings/NonRepeatingCharacter.java
@@ -8,17 +8,13 @@ public class NonRepeatingCharacter {
         int str1Length = str1.length();
         int str2Length = str2.length();
         for (int i = 0; i < str1Length; i++) {
-            if (i < str1Length) {
-                if (!str2.contains(String.valueOf(str1.charAt(i)))) {
-                    result += str1.charAt(i);
-                }
+            if (!str2.contains(String.valueOf(str1.charAt(i)))) {
+                result += str1.charAt(i);
             }
         }
         for (int i = 0; i < str2Length; i++) {
-            if (i < str2Length) {
-                if (!str1.contains(String.valueOf(str2.charAt(i)))) {
-                    result += str2.charAt(i);
-                }
+            if (!str1.contains(String.valueOf(str2.charAt(i)))) {
+                result += str2.charAt(i);
             }
         }
         System.out.println(result);
