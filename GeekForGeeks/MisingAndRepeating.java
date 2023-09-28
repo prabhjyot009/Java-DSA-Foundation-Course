f14cfc66d3e442f08837576f9b9756b3bad1eb19
prabhjyot009 <prabh156dps@gmail.com>
2023-09-29 01:07:20 +0530

    Missing and element in array

--- GeekForGeeks/MisingAndRepeating.java
+++ GeekForGeeks/MisingAndRepeating.java
@@ -2,37 +2,36 @@ package GeekForGeeks;
 
 public class MisingAndRepeating {
     static void printArray(int[] arr,int n){
-        for (int i = 0; i < n; i++) {
+        for(int i=0;i<n;i++){
             System.out.print(arr[i]+" ");
         }
         System.out.println();
     }
-    static void findTwoElements(int[] arr,int n){
+    static void findTwoElement(int[] arr,int n){
         int repeating=0;
         int missing=0;
         for (int i = 0; i < n; i++) {
-            int idx=Math.abs(arr[i]);
-            if(arr[idx]>0){
+            int idx=Math.abs(arr[i])-1;
+            if(arr[idx]<0){
+                repeating=idx+1;
+            }else{
                 arr[idx]=-arr[idx];
-            }
-            else{
-                repeating=arr[idx];
             }
         }
         for (int i = 0; i < n; i++) {
             if(arr[i]>0){
                 missing=i+1;
+                break;
             }
         }
-        System.out.println("repeating: "+repeating+" missing: "+missing);
+        System.out.println("Repeating element is:"+repeating);
+        System.out.println("Missing element is:"+missing);
     }
     public static void main(String[] args) {
-        int[] arr={2,2,1};
+        int[] arr={2,2};
         int n=arr.length;
-        System.out.println("orignal array:");
-        printArray(arr, n);
-        findTwoElements(arr, n);
-        System.out.println("after finding repeating and missing elements:");
-        printArray(arr, n);
+        System.out.println("Orignal array:");
+        printArray(arr,n);
+        findTwoElement(arr,n);
     }
 }
