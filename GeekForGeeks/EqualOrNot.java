188c5bf8db9862c2b579afb5e251b21fb6b6185b
prabhjyot009 <prabh156dps@gmail.com>
2023-09-28 23:02:00 +0530

    Equal or not

--- GeekForGeeks/EqualOrNot.java
+++ GeekForGeeks/EqualOrNot.java
@@ -7,6 +7,9 @@ public class EqualOrNot {
     static boolean check(long[] arr1,long[] arr2){
         long arr1Length=arr1.length;
         long arr2Length=arr2.length;
+        if(arr1Length!=arr2Length){
+            return false;
+        }
         Arrays.sort(arr1);
         Arrays.sort(arr2);
         for (int i = 0; i < arr1.length; i++) {
@@ -19,10 +22,13 @@ public class EqualOrNot {
     public static void main(String[] args) {
         long[] arr1={1,2,5,4,0};
         long[] arr2={2,4,5,0,1};
-        System.out.println("Output:");
+        System.out.print("Equal! : ");
+        check(arr1, arr2);
         if(check(arr1,arr2)){
+            System.out.println("yes");
         }
         else{
+            System.out.println("no");
         }
     }
 }
