ff9f25edb7e1f3e69dc0eba8987d642756587f9c
prabhjyot009 <prabh156dps@gmail.com>
2023-10-01 00:38:04 +0530

    Recursive Linked List

---
+++ LinkedList/RecursiveDisplayLinkedList.java
@@ -0,0 +1,31 @@
+package LinkedList;
+
+public class RecursiveDisplayLinkedList {
+    public static void display(Node head){
+        Node temp=head;
+        while(temp!=null){
+            System.out.print(temp.data+" ");
+            temp=temp.next;
+        }
+    }
+    public static class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data=data;
+        }
+    }
+    public static void main(String[] args) {
+        Node a=new Node(5);
+        Node b=new Node(3);
+        Node c=new Node(9);
+        Node d=new Node(8);
+        Node e=new Node(16);
+
+        a.next=b; 
+        b.next=c;
+        c.next=d;
+        d.next=e;
+        display(a);
+    }
+}
