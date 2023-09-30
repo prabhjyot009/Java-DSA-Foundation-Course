ae8db121bed9f789c51c8e44d2c79a952ec49932
prabhjyot009 <prabh156dps@gmail.com>
2023-10-01 01:58:53 +0530

    Length of linked list

---
+++ LinkedList/LengthLL.java
@@ -0,0 +1,35 @@
+package LinkedList;
+
+public class LengthLL {
+    public static class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data=data;
+        }
+    }
+    public static int length(Node head){
+        int count=0;
+        while(head!=null){
+            count++;
+            head=head.next;
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        Node a=new Node(5);
+        Node b=new Node(3);
+        Node c=new Node(9);
+        Node d=new Node(8);
+        Node e=new Node(16);
+        Node f=new Node(20);
+
+        a.next=b; 
+        b.next=c;
+        c.next=d;
+        d.next=e;
+        e.next=f;
+        Node head=a;
+        System.out.println(length(head));
+    }
+}
---
+++ LinkedList/implementationLL.java
@@ -0,0 +1,69 @@
+package LinkedList;
+
+public class implementationLL {
+    public static class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data=data;
+        }
+    }
+    public static class linkedlist{ //data structure
+        Node head=null; //user defined dataype
+        Node tail=null;
+        //creating function insert at end:
+        void insertAtEnd(int val){
+            Node temp=new Node(val);
+            if(head==null){//empty list
+                head=temp;
+            }
+            else{
+                tail.next=temp;  //head
+                // tail=temp; //we remove this from here because it is redundant
+            }
+            tail=temp; //we will use this here because it will apply to both if and else statement
+        }
+        //insert at beginning:
+        void insertAtHead(int val){
+            Node temp=new Node(val);
+            if(head==null){
+                // head=tail=temp;
+                //or
+                insertAtEnd(val);
+            }
+            else{ //non empty list
+                temp.next=head;  //next head will be displayed 
+                head=temp;
+            }
+        }
+        //for display:
+        void display(){  //no need of parameter here(Node head)
+            Node temp=head;
+            while(temp!=null){
+                System.out.print(temp.data+" ");
+                temp=temp.next;
+            }
+            System.out.println();
+        }
+        int size(){
+            Node temp=head;
+            int count=0;
+            while(temp!=null){
+                count++;
+                temp=temp.next;
+            }        
+            return count;
+        }
+    }
+    public static void main(String[] args) {
+        linkedlist ll=new linkedlist();
+        ll.insertAtEnd(4); // 4 
+        ll.display();
+        ll.insertAtEnd(5); // 4 -> 5 
+        ll.display();
+        ll.insertAtEnd(12);
+        ll.display();
+        ll.insertAtHead(0);
+        ll.display();
+    }
+}
