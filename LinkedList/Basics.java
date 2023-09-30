bb465ac8b9869cabef27062c8dee4ca68ef49eae
prabhjyot009 <prabh156dps@gmail.com>
2023-09-30 21:48:21 +0530

    basics of linked list

--- LinkedList/Basics.java
+++ LinkedList/Basics.java
@@ -1,5 +1,36 @@
 package LinkedList;
 
 public class Basics {
-    
+    public static class Node{
+         int data;
+         Node next;//address of next
+         Node(int data){
+            this.data=data;
+         }
+    }
+    public static void main(String[] args) {
+        // Node x = new Node();
+        // System.out.println(x);
+        // System.out.println(x.next);
+
+        Node a= new Node(5);
+        System.out.println(a.next);//null
+        Node b= new Node(3);
+        Node c= new Node(9);
+        Node d= new Node(8);
+        Node e= new Node(16);
+        //5->3->9->8->16;
+        a.next=b;//a.next me b node ka address.
+        System.out.println(a);//$Node@5acf9800
+        System.out.println(a.next);//$Node@4617c264
+        System.out.println(b);//$Node@4617c264        
+        System.out.println(c);//$Node@36baf30c
+        
+        System.out.println(a.data);
+        System.out.println(a.next.data);
+
+        b.next=c;
+        c.next=d;
+        d.next=e; 
+    }
 }
