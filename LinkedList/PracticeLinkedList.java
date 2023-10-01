e3ff226668dff431c15282936bd20a37c1fe42b9
prabhjyot009 <prabh156dps@gmail.com>
2023-10-02 02:39:56 +0530

    commit

---
+++ LinkedList/PracticeLinkedList.java
@@ -0,0 +1,54 @@
+package LinkedList;
+
+import LinkedList.DeleteAtIndexLL.linkedlist;
+
+public class PracticeLinkedList {
+    public static class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data=data;
+        }
+    }
+    public static class linkedlist{
+        Node head=null;
+        Node tail=null;
+        void insertAtEnd(int val){
+            Node temp=new Node(val);
+            if(head==null){
+                head=temp;
+            }
+            else{
+                tail.next=temp;
+            }
+            tail=temp;
+        }
+        void insertAtHead(int val){
+            Node temp=new Node(val);
+            if(head==null){
+                head=tail=temp;
+            }
+            else{
+                temp.next=head;
+                head=temp;
+            }
+        }
+        void display(){
+            Node temp=head;
+            while(temp!=null){
+                System.out.print(temp.data+" ");
+                temp=temp.next;
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        linkedlist ll=new linkedlist();
+        ll.insertAtEnd(1);
+        ll.insertAtEnd(2);
+        ll.insertAtEnd(3);
+        ll.insertAtEnd(4);
+        ll.insertAtHead(0);
+        ll.display();
+    }
+}
