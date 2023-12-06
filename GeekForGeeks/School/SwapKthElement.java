d646c502f1325f89419e6992761e4f6b2234fbfb
prabhjyot009 <prabh156dps@gmail.com>
2023-12-07 00:49:42 +0530

    Swap Kth element in array

--- GeekForGeeks/School/SwapKthElement.java
+++ GeekForGeeks/School/SwapKthElement.java
@@ -2,6 +2,14 @@ package GeekForGeeks.School;
 
 public class SwapKthElement {
     public static void main(String[] args) {
-        
+        int Arr[]={1, 2, 3, 4, 5, 6, 7, 8};
+        int n=Arr.length;
+        int k=3;
+        int temp=Arr[k-1];
+        Arr[k-1]=Arr[n-k];
+        Arr[n-k]=temp;
+        for (int i = 0; i < Arr.length; i++) {
+            System.out.print(Arr[i]+" ");
+        }
     }
 }
