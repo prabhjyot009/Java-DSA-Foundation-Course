67931438483cf22ee70803e2751aad20b44c9cff
prabhjyot009 <prabh156dps@gmail.com>
2024-01-18 14:26:21 +0530

    c

--- CP/Permutations.java
+++ CP/Permutations.java
@@ -1,13 +1,42 @@
 package CP;
 
-public class Permutations {
+// public class Permutations {
+//     public static int fact(String str){
+//         int length=str.length();
+//         int count=1;
+//         for (int i = 1; i <=length; i++) {
+//             count=count*i;
+//         }
+//         return count;
+//     }
+//     public static 
+//     public static void main(String[] args) {
+//         String str="abc";
+//         System.out.println(fact(str));
+//     }
+// }
+
+//using hashset
+
+import java.util.*;
+public class Permutations
+{
     public static void main(String[] args) {
-        String str="abc";
-        int length=str.length();
-        int count=1;
-        for (int i = 1; i <=length; i++) {
-            count=count*i;
+        String str="eye";
+        HashSet<String> hs=new HashSet<>();
+        permutations(str,hs,"");
+        System.out.println(hs.size());
+    }
+    public static void permutations(String str,HashSet<String> hs,String ans){
+        if(str.length()==0){
+            hs.add(ans);
+            return;
         }
-        System.out.println(count);
+        for(int i=0;i<str.length();i++){
+            char ch=str.charAt(i);
+            String ros=str.substring(0,i)+str.substring(i+1);
+            permutations(ros,hs,ans+ch);
+        }
     }
 }
+
