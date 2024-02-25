5256d3c690fa636e2d115e694233b7d2ae6371ca
prabhjyot009 <prabh156dps@gmail.com>
2024-02-25 12:52:51 +0530

    Add launch configuration for reverseNum Java class

---
+++ StriverDSA/reverseNum.java
@@ -0,0 +1,13 @@
+package StriverDSA;
+
+public class reverseNum {
+    public static void main(String[] args) {
+        int num = 124;
+        int rev = 0;
+        while (num > 0) {
+            rev = (rev * 10) + (num % 10);
+            num = num / 10;
+        }
+        System.out.println(rev);
+    }
+}
