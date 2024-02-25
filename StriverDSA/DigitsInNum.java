886e8b5b1437f4b4625da6ba685b790e248878f3
prabhjyot009 <prabh156dps@gmail.com>
2024-02-25 12:38:51 +0530

    Add launch configuration for PartitionSubarrays and optimize DigitsInNum

--- StriverDSA/DigitsInNum.java
+++ StriverDSA/DigitsInNum.java
@@ -1,5 +1,7 @@
 package StriverDSA;
 
+import java.math.*;
+
 public class DigitsInNum {
     public static void main(String[] args) {
         int num = 1004;
@@ -9,6 +11,9 @@ public class DigitsInNum {
             count++;
             num /= 10;
         }
-        System.out.println(count);
+        // using log
+        int nume = 1004;
+        int digits = (int)Math.log10(nume) + 1;
+        System.out.println("Digits in number using log: " + digits);
     }
 }
