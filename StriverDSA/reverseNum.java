e00819d6ccd10538fa1b5d9a50b54f40440ac84b
prabhjyot009 <prabh156dps@gmail.com>
2024-02-25 12:53:24 +0530

    Update reverseNum.java to reverse a larger number

--- StriverDSA/reverseNum.java
+++ StriverDSA/reverseNum.java
@@ -2,7 +2,7 @@ package StriverDSA;
 
 public class reverseNum {
     public static void main(String[] args) {
-        int num = 124;
+        int num = 102400;
         int rev = 0;
         while (num > 0) {
             rev = (rev * 10) + (num % 10);
