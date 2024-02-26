e08ee4a86ac46efec9ab9a28556e27e828cfda4f
prabhjyot009 <prabh156dps@gmail.com>
2024-02-27 00:33:05 +0530

    Add Java launch configurations

---
+++ StriverDSA/palindrome.java
@@ -0,0 +1,9 @@
+package StriverDSA;
+
+public class palindrome {
+    public static void main(String[] args) {
+
+        System.out.println();
+
+    }
+}
---
+++ stacks/stackbasics.java
@@ -0,0 +1,23 @@
+package stacks;//LIFO,FILO
+
+import java.util.Stack;
+
+public class stackbasics {
+    public static void main(String[] args) {
+        Stack<Integer> st=new Stack<>();
+        st.push(2);
+        st.push(34);
+        st.push(90);
+        st.push(5);
+        System.out.println(st.peek());
+        System.out.println(st);
+        st.pop();
+        System.out.println(st);
+        System.out.println(st.size());
+        while(st.size()>1){
+            st.pop();
+        }
+        System.out.println(st.peek());
+        System.out.println(st);
+    }
+} 
