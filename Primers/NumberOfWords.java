a7836d929449a9e90bbc1f358037abc8f6d47a53
prabhjyot009 <prabhjyot009s@gmail.com>
2024-07-03 11:38:36 +0530

    feat: Add functionality to count the number of words in a given string

--- Primers/NumberOfWords.java
+++ Primers/NumberOfWords.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class NumberOfWords {
     public static void main(String[] args) {
-        
+        int count=0;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter String:");
+        String str=sc.nextLine();
+        String strr=str.toLowerCase();
+        for (int i = 0; i < strr.length(); i++) {
+            if(strr.charAt(i)!=' '){
+                count++;
+            }
+        }
+        System.out.println(count);
     }
 }
